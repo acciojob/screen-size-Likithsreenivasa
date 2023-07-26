5c08662c07949b8aa269346bdddd7cfb4cde98eb
Likithsreenivasa <118590713+Likithsreenivasa@users.noreply.github.com>
2023-07-26 15:16:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function updateInfo()
 		const width=window.innerWidth;
 		const height=window.innerHeight;
 		const sizeInfo=document.getElementById("sizeInfo").querySelector("h1");
-		sizeInfo.textContent=`Width:${width} and Height:${height}`
+		sizeInfo.textContent=`Width: ${width} and Height: ${height}`
    }
 updateInfo();
 window.addEventListener("resize",updateInfo);
